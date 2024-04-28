e1f47f316700fa226e074e6ff904752b15b7c7bf
premh27 <164321843+premh27@users.noreply.github.com>
2024-04-29 00:18:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+//curl -SL https://github.com/docker/compose/releases/download/v2.24.6/do ckercompose-linux-x86_64 -o /usr/local/bin/docker-compose
+//chmod +x /usr/local/bin/docker-compose 
+// docker compose version
+//mkdir composetest
+//docker compose up
+import time
+import redis
+from flask import Flask
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+
+            retries -= 1
+            time.sleep(0.5)
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
